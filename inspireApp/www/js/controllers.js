950dc9bc457c75f547a51bdb7d540f2a55a071ef
ctrovato <trovato.carmine@gmail.com>
2015-06-07 23:37:48 -0400

    Started the Inspire application
    Created Ionic application and set up models and views for Add Task,
    Task List and Badges

---
+++ inspireApp/www/js/controllers.js
@@ -0,0 +1,8 @@
+angular.module('starter.controllers', [])
+
+.controller('AddCtrl', function($scope) {})
+
+.controller('ListCtrl', function($scope, $ionicListDelegate) {})
+
+.controller('BadgesCtrl', function($scope) {})
+
